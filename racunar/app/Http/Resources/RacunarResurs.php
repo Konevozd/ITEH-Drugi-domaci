025b659ded404fa336e174e91039d2d369994979
Konevozd <nemanja543210@gmail.com>
2023-04-25 17:13:51 +0200

    Modified resources

--- racunar/app/Http/Resources/RacunarResurs.php
+++ racunar/app/Http/Resources/RacunarResurs.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Proizvodjac;
+use App\Models\Tip;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RacunarResurs extends JsonResource
@@ -14,6 +16,16 @@ class RacunarResurs extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+
+        $tip = Tip::find($this->tipID);
+        $proizvodjac = Proizvodjac::find($this->proizvodjacID);
+
+        return [
+            'id' => $this->id,
+            'proizvodjac' => $proizvodjac->proizvodjac,
+            'model' => $this->model,
+            'tip' => $tip->tip,
+            'specifikacija' => $this->specifikacija
+        ];
     }
 }
